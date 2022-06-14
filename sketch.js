b8b321a57f8ff73e652077a5c69b74375c22ae48
Elliot Hipski <elhip03@gmail.com>
2022-06-14 16:40:58 -0400

    made the 2D array of pixels

--- sketch.js
+++ sketch.js
@@ -1,10 +1,34 @@
+const board = document.getElementById("sketchboard");
+
 const divList = [];
+const ArrayOfDivList = [];
 
 function createGrid () {
-    for (i = 0; i < 16; i++) {
-        let d = document.createElement("div");
-        divList[i] = d;
+    for (let j = 0; j < 16; j++) {
+        ArrayOfDivList[j] = document.createElement("div");
+        document.getElementById("Sketchboard").appendChild(ArrayOfDivList[j]);
+        
+        for (let i = 0; i < 16; i++) {
+
+            divList[i] = document.createElement("div");
+            ArrayOfDivList[j].appendChild(divList[i]);
+
+            divList[i].style.display = "flex";
+            
+            divList[i].style.height = "10px";
+            divList[i].style.width = "10px";
+            divList[i].style.backgroundColor = "red";
+
+            
+
+            
+            console.log("pix appended");
+        }
+        ArrayOfDivList[j] = divList;
     }
+
+
 }
 
 
+createGrid();
