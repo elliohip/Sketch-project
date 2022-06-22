11b74fc69a6889a046336a873e6b83c67028a2f7
Elliot Hipski <elhip03@gmail.com>
2022-06-21 22:34:56 -0400

    made flexible grid

--- sketch.js
+++ sketch.js
@@ -6,42 +6,79 @@ const ArrayOfDivList = [];
 let input = document.getElementById("color");
 let colorButton = document.getElementById("submit-button");
 
-let gameColor;
+let gameColor = "black";
 let width;
 let height;
-let size = width * height;
 
-function setColor(color) {
-    gameColor = color;
-}
+let colorChecker = (gameColor != undefined);
 
+
+
+
+function changeColor(ev) {
+
+    ev.currentTarget.backgroundColor = gameColor;
+
+};
+
+
+
+function setColor() {
+    gameColor = document.getElementById("color").value;
+
+    colorChecker = true;
+
+    console.log(gameColor);
+};
+
+colorButton.addEventListener("click", setColor);
 
 
 
 
 function createGrid (x, y) {
 
+    let pixnum = x*y;
+
+    let pixw = 1/x * 100;
+    let pixh = 1/y * 100;
+
+    
+
     for (let j = 0; j < x; j++) {
         
-        let row = document.createElement("div");
+        
 
-        document.getElementById("Sketchboard").appendChild(row);
+        
         
         for (let i = 0; i < y; i++) {
+
             
             
             
             divList[i] = document.createElement("div");
 
-            divList[i].style.display = "flex";
+
+            divList[i].style.display.alignSelf = "center";
+
+
+            divList[i].style.backgroundColor = "black";
+
             
-            divList[i].style.height = "1px";
-            divList[i].style.width = "1px";
-            divList[i].style.backgroundColor = "red";
+            divList[i].style.borderColor = "black";
+            divList[i].style.borderWidth = "1px";
 
-            divList[i];
+            divList[i].style.width = pixw + "%";
+            divList[i].style.height = pixh + "%";
+            
 
-            row.appendChild(divList[i]);
+
+            divList[i].addEventListener("mouseover", changeColor)
+
+
+        
+
+            document.getElementById("Sketchboard").appendChild(divList[i]);
 
 
             
@@ -49,9 +86,10 @@ function createGrid (x, y) {
             
             console.log("pix appended");
         }
+
         ArrayOfDivList[j] = divList;
         
-        
+        console.log("made Row");
     }
 
 
@@ -64,4 +102,10 @@ function askSize() {
 }
 
 askSize();
-createGrid(width, height);
+
+
+
+
+if (colorChecker) {
+    createGrid(width, height);
+}
